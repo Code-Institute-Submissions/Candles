c5319dd36f08f0d5d647180ff6433387a5eca32a
Wayne Pegg <peggy161152@gmail.com>
2020-05-01 12:59:56 +0100

    Shop app updated with urls/views

--- candles/urls.py
+++ candles/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('home.urls')),
     path('home/', include('home.urls')),
+    path('shop/', include('shop.urls')),
     path('admin/', admin.site.urls),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def displayProducts(request):
+    return render(request, 'shop/store.html')
