5aa65cdde00af4081a3f5ba16077e44aff1542aa
Wayne Pegg <peggy161152@gmail.com>
2020-05-27 17:43:31 +0100

    Stripe Checkout now complete. Starting Snipcart integration

--- payment/views.py
+++ payment/views.py
@@ -21,6 +21,6 @@ def charge(request):
    session = stripe.checkout.Session.create(
         payment_method_types=['card'],line_items=[{'price': '{{PRICE_ID}}','quantity': 1,}],
         mode='payment',
-        success_url='https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
-        cancel_url='https://example.com/cancel',
+        success_url='http://127.0.1:8000/payment/success?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url='http://127.0.1:8000/payment/cancel/',
     )
