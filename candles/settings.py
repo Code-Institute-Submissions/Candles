5b806dddd24f8b4706085803388116843ce131c1
Wayne Pegg <peggy161152@gmail.com>
2020-06-12 16:29:45 +0100

    updated having tested. PostgresQL updated and linked

--- candles/settings.py
+++ candles/settings.py
@@ -94,11 +94,23 @@ WSGI_APPLICATION = 'candles.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'candles',
+        'USER': 'postgres',
+        'PASSWORD': 'Gizmo1974!',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+"""
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
