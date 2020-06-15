dea33ffeca64a2a905f54a49f9980564a10e6d0d
Wayne Pegg <peggy161152@gmail.com>
2020-06-15 16:44:56 +0100

    Commencing testing using unittests and coverage.py

---
+++ contact/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class SendEmailMessage(forms.Form):
+    message_name = forms.CharField(max_length=50, label_suffix='Your name', required=True, widget=forms.TextInput) 
+    message_email = forms.EmailField(max_length=50)
+    message_body = forms.CharField(max_length=1000, required=True, label_suffix='Your message here please', help_text='1000 characters maximum.', widget=forms.TextInput)
--- contact/views.py
+++ contact/views.py
@@ -1,44 +1,47 @@
 from django.conf import settings
 from django.shortcuts import render
-from django.core.mail import send_mail, EmailMessage
+from django.core.mail import EmailMessage
 
+from .forms import SendEmailMessage
 
 def emailContact(request):
     
     if request.method == 'POST':
-        message_name = request.POST['message-name']
-        message_email = request.POST['message-email']
-        message_body = request.POST['message-body']
-        context = {'menu_class': 'menu-login', 'message_name': message_name}
+        form = SendEmailMessage(request.POST)
+        if form.is_valid():
+            message_name = form.cleaned_data['message_name']
+            to_email = form.cleaned_data['message_email']
+            message_body = form.cleaned_data['message_body']
+            context = {'menu_class': 'menu-login', 'message_name': message_name}
+            """
+            These emails are sent to the client who has filled in the online email
+            contact form on the Abount&Contact Page. A sendinblue template email
+            is sent letting the client know their email has been received and 
+            offers them information about a new product being launched. 'message2'
+            is an email that is sent to the companies email address with the 
+            clients message included and contact details.
+            """
+            message = EmailMessage(
+                subject=str(message_name),
+                body=str(message_body),
+                to=[str(to_email)],
+                )
+            message.template_id = 1  # use this Sendinblue template
+            message.from_email = None  # to use the template's default sender
+            message.merge_global_data = {'name': message_name,}
+            message.send()
 
-        """
-        These emails are sent to the client who has filled in the online email
-        contact form on the Abount&Contact Page. A sendinblue template email
-        is sent letting the client know their email has been received and 
-        offers them information about a new product being launched. 'message2'
-        is an email that is sent to the companies email address with the 
-        clients message included and contact details.
-        """
-        message = EmailMessage(
-            subject=str(message_name),
-            body=str(message_body),
-            to=[str(message_email)],
+            message2 = EmailMessage(
+                subject="Message sent from website by "+str(message_name),
+                body = str(message_body),
+                from_email=str(to_email),
+                to=['oandxcandles@gmail.com'],
             )
-        message.template_id = 1  # use this Sendinblue template
-        message.from_email = None  # to use the template's default sender
-        message.merge_global_data = {'name': message_name,}
-        message.send()
-
-        message2 = EmailMessage(
-            subject="Message sent from website by "+str(message_name),
-            body = str(message_body),
-            from_email=str(message_email),
-            to=['oandxcandles@gmail.com'],
-        )
-        message2.send()
-        return render(request, 'contact/emailSent.html', context)
+            message2.send()
+            return render(request, 'contact/emailSent.html', context)
     else:
-        context = {'menu_class': 'menu-login'}
+        form = SendEmailMessage()
+        context = {'menu_class': 'menu-login', 'form': form}
         return render(request, 'contact/email.html', context)
 
 def emailSentConfirm(request):
